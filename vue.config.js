bfc932e247f61d88d5cb7398f978e749ce4bc422
水华 <mizuka.wu@outlook.com>
2021-01-07 00:21:16 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -7,15 +7,7 @@ module.exports = {
             target: 'dir',
             arch: ['arm64', 'x64']
           }
-        },
-        publish: [
-          {
-            provider: 'github',
-            owner: 'mizuka-wu',
-            repo: 'kanban',
-            releaseType: 'release'
-          }
-        ]
+        }
       },
       nodeIntegration: true
     }
