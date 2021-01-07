ca7b06459cf4a4545ef7e0491fc6492e668acc7b
水华 <mizuka.wu@outlook.com>
2021-01-07 13:06:39 +0800

    fix: publish

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-template-curly-in-string */
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        appId: 'top.mizuka.kanban',
+        artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
         publish: ['github']
+        // mac: {
+        //   target: [
+        //     {
+        //       arch: 'universal',
+        //       target: 'dmg'
+        //     },
+        //     {
+        //       arch: 'universal',
+        //       target: 'zip'
+        //     }
+        //   ]
+        // }
       },
       nodeIntegration: true
     }
